49f780db4de262f5c581e26828ebdb0e410b0614
ilmar <ilmar.danilo@querodelivery.com>
2023-01-08 13:26:34 -0300

    refactor: move production code to their own files

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -1,5 +1,6 @@
+import { FacebookApi } from '../../../src/infra/apis';
+import { HttpGetClient } from '../../../src/infra/http';
 import { mock, MockProxy } from 'jest-mock-extended';
-import { LoadFacebookUserApi } from '../../../src/data/contracts/api';
 
 describe('FacebookApi', () => {
   let clientId: string;
@@ -30,35 +31,3 @@ describe('FacebookApi', () => {
     });
   });
 });
-
-export class FacebookApi {
-  private readonly baseUrl = 'https://graph.facebook.com';
-
-  constructor(
-    private readonly httpClient: HttpGetClient,
-    private readonly clientId: string,
-    private readonly clientSecret: string,
-  ) {}
-
-  async loadUser(params: LoadFacebookUserApi.Params): Promise<void> {
-    await this.httpClient.get({
-      url: `${this.baseUrl}/oauth/access_token`,
-      params: {
-        client_id: this.clientId,
-        client_secret: this.clientSecret,
-        grant_type: 'client_credentials',
-      },
-    });
-  }
-}
-
-export interface HttpGetClient {
-  get: (params: HttpGetClient.Params) => Promise<void>;
-}
-
-export namespace HttpGetClient {
-  export type Params = {
-    url: string;
-    params: object;
-  };
-}

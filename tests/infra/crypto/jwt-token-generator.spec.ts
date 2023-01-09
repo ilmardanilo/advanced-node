75354f674f9ee76ab79827f7126a311b922d1908
ilmar <ilmar.danilo@querodelivery.com>
2023-01-08 22:40:56 -0300

    feat: ensure JwtTokenGenerator returns token on success

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -6,9 +6,11 @@ jest.mock('jsonwebtoken');
 
 class JwtTokenGenerator {
   constructor(private readonly secret: string) {}
-  async generateToken(params: TokenGenerator.Params): Promise<void> {
+  async generateToken(
+    params: TokenGenerator.Params,
+  ): Promise<TokenGenerator.Result> {
     const expirationInSeconds = params.expirationInMs / 1000;
-    jwt.sign({ key: params.key }, this.secret, {
+    return jwt.sign({ key: params.key }, this.secret, {
       expiresIn: expirationInSeconds,
     });
   }
@@ -20,6 +22,7 @@ describe('JwtTokenGenerator', () => {
 
   beforeAll(() => {
     fakeJwt = jwt as jest.Mocked<typeof jwt>;
+    fakeJwt.sign.mockImplementation(() => 'any_token');
   });
 
   beforeEach(() => {
@@ -34,5 +37,14 @@ describe('JwtTokenGenerator', () => {
       'any_secret',
       { expiresIn: 1 },
     );
+  });
+
+  it('should return a token on success', async () => {
+    const token = await sut.generateToken({
+      key: 'any_key',
+      expirationInMs: 1000,
+    });
+
+    expect(token).toBe('any_token');
   });
 });

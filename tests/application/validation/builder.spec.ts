1a24ce6564b88eb3b16a6effcf4f88d230e431f6
ilmar <ilmar.danilo@querodelivery.com>
2023-01-15 16:45:27 -0300

    refactor: move production code to its own file

--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,30 +1,7 @@
 import {
   RequiredStringValidator,
-  Validator,
+  ValidationBuilder,
 } from '../../../src/application/validation';
-
-class ValidationBuilder {
-  private constructor(
-    private readonly value: string,
-    private readonly fieldName: string,
-    private readonly validators: Validator[] = [],
-  ) {}
-
-  static of(params: { value: string; fieldName: string }): ValidationBuilder {
-    return new ValidationBuilder(params.value, params.fieldName);
-  }
-
-  required(): ValidationBuilder {
-    this.validators.push(
-      new RequiredStringValidator(this.value, this.fieldName),
-    );
-    return this;
-  }
-
-  build(): Validator[] {
-    return this.validators;
-  }
-}
 
 describe('ValidationBuilder', () => {
   it('should return a RequiredStringValidator', () => {

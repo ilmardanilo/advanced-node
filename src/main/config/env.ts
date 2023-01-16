747d0b3b3fb481fc830d2044ab75225e870faf3e
ilmar <ilmar.danilo@querodelivery.com>
2023-01-16 18:54:33 -0300

    feat: ensure FacebookApi returns undefined when HttpGetClient throws

---
+++ src/main/config/env.ts
@@ -0,0 +1,7 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FB_CLIENT_ID || '731188411760647',
+    clientSecret:
+      process.env.FB_CLIENT_SECRET || 'a53ad2e315017ace553cd85dfe61fa39',
+  },
+};
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,3 +1,40 @@
-describe('', () => {
-  it('', () => {});
+import { FacebookApi } from '../../src/infra/apis';
+import { AxiosHttpClient } from '../../src/infra/http';
+import { env } from '../../src/main/config/env';
+
+describe('Facebook Api Integration Tests', () => {
+  it('should return a Facebook User if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient();
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret,
+    );
+
+    const fbUser = await sut.loadUser({
+      token:
+        'EAAKZAAxGMAAcBADU5QN5atiZCVeDLF1dSw7VcdSZB51KFMXmlujjiNrKZABAlPlozslzn5CZAHS8RJMHMtVI6P3pianloqhhCQg0NoD8bRZAw5r7S7bIr8wbtqQvymKpnYVgy5aNUIytSkryh4mZByVz08T2LhX0KYtI3rVyhyl86BSJwaiqoSUvYQgP7IMf69lRUGDn3X6IaqXTLHJY9YL',
+    });
+
+    expect(fbUser).toEqual({
+      facebookId: '105513015776378',
+      name: 'Ilmar Teste',
+      email: 'ilmar_pvzvkwx_teste@tfbnw.net',
+    });
+  });
+
+  it('should return a undefined if token is invalid', async () => {
+    const axiosClient = new AxiosHttpClient();
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret,
+    );
+
+    const fbUser = await sut.loadUser({
+      token: 'invalid',
+    });
+
+    expect(fbUser).toBeUndefined();
+  });
 });

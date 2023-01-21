1e8e170568032cb24b4b20d5f9095e61bf905ad1
ilmar <ilmar.danilo@querodelivery.com>
2023-01-21 17:44:39 -0300

    chore: connect to postgres db before server init

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,12 @@
+import { createConnection } from 'typeorm';
+import { config } from '../infra/postgres/helpers';
 import { app } from './config/app';
 import { env } from './config/env';
 
-app.listen(env.port, () => {
-  console.log(`Server running at http://localhost:${env.port}`);
-});
+createConnection(config)
+  .then(() => {
+    app.listen(env.port, () => {
+      console.log(`Server running at http://localhost:${env.port}`);
+    });
+  })
+  .catch(console.error);

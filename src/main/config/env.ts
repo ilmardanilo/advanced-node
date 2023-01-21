b3e9826e96194689da07d16f71349af70b59f48f
ilmar <ilmar.danilo@querodelivery.com>
2023-01-21 12:28:20 -0300

    feat: add Facebook Api factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -5,4 +5,5 @@ export const env = {
       process.env.FB_CLIENT_SECRET || 'a53ad2e315017ace553cd85dfe61fa39',
   },
   port: process.env.PORT || 3333,
+  jwtSecret: process.env.JWT_SECRET || '63617d955442ca004584b1ed',
 };

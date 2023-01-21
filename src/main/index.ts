eb36948515fe16544fbe38cb7aea2d0c26e974db
ilmar <ilmar.danilo@querodelivery.com>
2023-01-21 19:02:52 -0300

    refactor: move typeorm config to root folder

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,8 @@
 import { createConnection } from 'typeorm';
-import { config } from '../infra/postgres/helpers';
 import { app } from './config/app';
 import { env } from './config/env';
 
-createConnection(config)
+createConnection()
   .then(() => {
     app.listen(env.port, () => {
       console.log(`Server running at http://localhost:${env.port}`);

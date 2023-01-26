e111dd65f902a3ea117d37a5947afa1567aa7119
ilmar <ilmar.danilo@querodelivery.com>
2023-01-26 19:56:14 -0300

    refactor: move production code to its own file

---
+++ src/domain/services/change-profile-picture.ts
@@ -0,0 +1,16 @@
+import { UUIDGenerator } from '../contracts/crypto';
+import { UploadFile } from '../contracts/gateways';
+
+type Params = { id: string; file: Buffer };
+
+export class ChangeProfilePictureService {
+  constructor(
+    private readonly fileStorage: UploadFile,
+    private readonly crypto: UUIDGenerator,
+  ) {}
+
+  async perform({ id, file }: Params): Promise<void> {
+    const uuid = this.crypto.uuid({ key: id });
+    await this.fileStorage.upload({ file, key: uuid });
+  }
+}
--- tests/domain/services/change-profile-picture.spec.ts
+++ tests/domain/services/change-profile-picture.spec.ts
@@ -1,35 +1,7 @@
+import { ChangeProfilePictureService } from '../../../src/domain/services';
+import { UUIDGenerator } from '../../../src/domain/contracts/crypto';
+import { UploadFile } from '../../../src/domain/contracts/gateways';
 import { mock, MockProxy } from 'jest-mock-extended';
-
-type Params = { id: string; file: Buffer };
-
-export class ChangeProfilePictureService {
-  constructor(
-    private readonly fileStorage: UploadFile,
-    private readonly crypto: UUIDGenerator,
-  ) {}
-
-  async perform({ id, file }: Params): Promise<void> {
-    const uuid = this.crypto.uuid({ key: id });
-    await this.fileStorage.upload({ file, key: uuid });
-  }
-}
-
-export interface UploadFile {
-  upload(params: UploadFile.Params): Promise<void>;
-}
-
-export namespace UploadFile {
-  export type Params = { file: Buffer; key: string };
-}
-
-export interface UUIDGenerator {
-  uuid(params: UUIDGenerator.Params): UUIDGenerator.Result;
-}
-
-export namespace UUIDGenerator {
-  export type Params = { key: string };
-  export type Result = string;
-}
 
 describe('ChangeProfilePictureService', () => {
   let uuid: string;

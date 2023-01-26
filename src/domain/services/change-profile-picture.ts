795a96b5e07e5d96181c02fcc1e34a7aaeb999b2
ilmar <ilmar.danilo@querodelivery.com>
2023-01-26 20:00:25 -0300

    feat: ensure ChangeProfilePictureService dont call UploadFile when file is undefined

--- src/domain/services/change-profile-picture.ts
+++ src/domain/services/change-profile-picture.ts
@@ -1,7 +1,7 @@
 import { UUIDGenerator } from '../contracts/crypto';
 import { UploadFile } from '../contracts/gateways';
 
-type Params = { id: string; file: Buffer };
+type Params = { id: string; file?: Buffer };
 
 export class ChangeProfilePictureService {
   constructor(
@@ -10,7 +10,9 @@ export class ChangeProfilePictureService {
   ) {}
 
   async perform({ id, file }: Params): Promise<void> {
-    const uuid = this.crypto.uuid({ key: id });
-    await this.fileStorage.upload({ file, key: uuid });
+    if (file) {
+      const uuid = this.crypto.uuid({ key: id });
+      await this.fileStorage.upload({ file, key: uuid });
+    }
   }
 }

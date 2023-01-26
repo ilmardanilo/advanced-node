82067ba68b3b4baac47762729f188e0322fc51ba
ilmar <ilmar.danilo@querodelivery.com>
2023-01-25 21:48:53 -0300

    feat: ensure ChangeProfilePictureService calls UploadFile with unique id

--- tests/domain/services/change-profile-picture.spec.ts
+++ tests/domain/services/change-profile-picture.spec.ts
@@ -3,10 +3,14 @@ import { mock } from 'jest-mock-extended';
 type Params = { id: string; file: Buffer };
 
 export class ChangeProfilePictureService {
-  constructor(private readonly fileStorage: UploadFile) {}
+  constructor(
+    private readonly fileStorage: UploadFile,
+    private readonly crypto: UUIDGenerator,
+  ) {}
 
   async perform({ id, file }: Params): Promise<void> {
-    await this.fileStorage.upload({ file, key: id });
+    const uuid = this.crypto.uuid({ key: id });
+    await this.fileStorage.upload({ file, key: uuid });
   }
 }
 
@@ -18,15 +22,27 @@ export namespace UploadFile {
   export type Params = { file: Buffer; key: string };
 }
 
+export interface UUIDGenerator {
+  uuid(params: UUIDGenerator.Params): UUIDGenerator.Result;
+}
+
+export namespace UUIDGenerator {
+  export type Params = { key: string };
+  export type Result = string;
+}
+
 describe('ChangeProfilePictureService', () => {
   it('should call UploadFile with correct params', async () => {
+    const uuid = 'any_unique_id';
     const file = Buffer.from('any_buffer');
     const fileStorage = mock<UploadFile>();
-    const sut = new ChangeProfilePictureService(fileStorage);
+    const crypto = mock<UUIDGenerator>();
+    crypto.uuid.mockReturnValue(uuid);
+    const sut = new ChangeProfilePictureService(fileStorage, crypto);
 
     await sut.perform({ id: 'any_id', file });
 
-    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: 'any_id' });
+    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: uuid });
     expect(fileStorage.upload).toHaveBeenCalledTimes(1);
   });
 });

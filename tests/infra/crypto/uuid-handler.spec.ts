5e73f1c5058715a135fa21ab136ceb7374ecb3d4
ilmar <ilmar.danilo@querodelivery.com>
2023-01-28 10:33:51 -0300

    feat: ensure UUIDHandler returns correct value

--- tests/infra/crypto/uuid-handler.spec.ts
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -1,11 +1,12 @@
 import { UUIDGenerator } from '../../../src/domain/contracts/crypto';
 import { v4 } from 'uuid';
+import { mocked } from 'ts-jest/utils';
 
 jest.mock('uuid');
 
 export class UUIDHandler {
-  uuid({ key }: UUIDGenerator.Params): void {
-    v4();
+  uuid({ key }: UUIDGenerator.Params): UUIDGenerator.Result {
+    return `${key}_${v4()}`;
   }
 }
 
@@ -16,5 +17,14 @@ describe('UUIDHandler', () => {
     sut.uuid({ key: 'any_key' });
 
     expect(v4).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return correct uuid', () => {
+    mocked(v4).mockReturnValueOnce('any_uuid');
+    const sut = new UUIDHandler();
+
+    const uuid = sut.uuid({ key: 'any_key' });
+
+    expect(uuid).toBe('any_key_any_uuid');
   });
 });

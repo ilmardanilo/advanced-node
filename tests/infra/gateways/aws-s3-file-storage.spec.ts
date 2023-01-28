4cac056f5c86647f996eff465a1ea1098f887bfd
ilmar <ilmar.danilo@querodelivery.com>
2023-01-28 16:20:47 -0300

    refactor: move production code to its own file

--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -1,35 +1,9 @@
-import { UploadFile } from '../../../src/domain/contracts/gateways';
+import { AwsS3FileStorage } from '../../../src/infra/gateways';
+
 import { config, S3 } from 'aws-sdk';
 import { mocked } from 'ts-jest/utils';
 
 jest.mock('aws-sdk');
-
-export class AwsS3FileStorage implements UploadFile {
-  constructor(
-    accessKey: string,
-    secret: string,
-    private readonly bucket: string,
-  ) {
-    config.update({
-      credentials: {
-        accessKeyId: accessKey,
-        secretAccessKey: secret,
-      },
-    });
-  }
-
-  async upload({ key, file }: UploadFile.Params): Promise<UploadFile.Result> {
-    const s3 = new S3();
-    await s3.putObject({
-        Bucket: this.bucket,
-        Key: key,
-        Body: file,
-        ACL: 'public-read',
-      }).promise();
-
-      return `https://${this.bucket}.s3.amazonaws.com/${encodeURIComponent(key)}`
-  }
-}
 
 describe('AwsS3FileStorage', () => {
   let accessKey: string;

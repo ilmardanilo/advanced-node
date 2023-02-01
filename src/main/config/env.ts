df95e7ae9bd29f705b5b3c2c72dc196dd22476c3
ilmar <ilmar.danilo@querodelivery.com>
2023-01-31 21:37:34 -0300

    test: add aws s3 integration test

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -7,6 +7,11 @@ export const env = {
       process.env.FB_ACCESS_TOKEN ||
       'EAAKZAAxGMAAcBADU5QN5atiZCVeDLF1dSw7VcdSZB51KFMXmlujjiNrKZABAlPlozslzn5CZAHS8RJMHMtVI6P3pianloqhhCQg0NoD8bRZAw5r7S7bIr8wbtqQvymKpnYVgy5aNUIytSkryh4mZByVz08T2LhX0KYtI3rVyhyl86BSJwaiqoSUvYQgP7IMf69lRUGDn3X6IaqXTLHJY9YL',
   },
+  s3: {
+    accessKey: process.env.AWS_S3_ACCESS_KEY || '',
+    secret: process.env.AWS_S3_SECRET || '',
+    bucket: process.env.AWS_S3_BUCKET || '',
+  },
   port: process.env.PORT || 3333,
   jwtSecret: process.env.JWT_SECRET || '63617d955442ca004584b1ed',
 };

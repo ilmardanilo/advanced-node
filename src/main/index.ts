038d06b802270457bdfaad18237f92bff6320b47
ilmar <ilmar.danilo@querodelivery.com>
2023-02-05 22:27:41 -0300

    fix: connection problems

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,13 @@
-import { createConnection } from 'typeorm';
-import { app } from './config/app';
+import { createConnection, getConnectionOptions } from 'typeorm';
+
 import { env } from './config/env';
 
-createConnection()
-  .then(() => {
+getConnectionOptions()
+  .then(async (options) => {
+    const root = process.env.TS_NODE_DEV === undefined ? 'dist' : 'src';
+    const entities = [`${root}/infra/postgres/entities/index.{js,ts}`];
+    await createConnection({ ...options, entities });
+    const { app } = await import('./config/app');
     app.listen(env.port, () => {
       console.log(`Server running at http://localhost:${env.port}`);
     });
